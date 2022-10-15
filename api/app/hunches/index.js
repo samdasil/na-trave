40829e71289d56122187013e07d1a1ccd8731d06
Sammy Silva <silvasammy@outlook.com>
2022-10-15 00:01:28 -0400

    feat: backend ok

---
+++ api/app/hunches/index.js
@@ -0,0 +1,49 @@
+import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken'
+
+const prisma = new PrismaClient()
+
+export const create = async (ctx) => {
+    
+    if ( !ctx.headers?.authorization ) {
+        ctx.status = 401
+        return
+    }
+
+    const [type, token] = ctx.headers.authorization.split(" ")
+
+    try{
+
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+
+        if ( !ctx.request.body.homeTeamScore && !ctx.request.body.awayTeamScore ) {
+            ctx.status = 400
+            return
+        }
+        
+        const userId = data.sub
+        const { gameId } = ctx.request.body
+        const homeTeamScore = parseInt(ctx.request.body.homeTeamScore)
+        const awayTeamScore = parseInt(ctx.request.body.awayTeamScore)
+        
+        try {
+            const hunch = await prisma.hunch.upsert({
+                where: { id: `${userId}${gameId}` },
+                create: { userId, gameId, homeTeamScore, awayTeamScore },
+                update: { homeTeamScore, awayTeamScore }
+            })
+    
+            ctx.body = hunch
+            ctx.status = 200
+        } catch (error) {
+            console.log(error);
+            ctx.body = error
+            ctx.status = 500
+        }
+    
+    } catch (error) {
+        ctx.status = 401
+        return
+    }
+
+}
---
+++ api/app/users/index.js
@@ -0,0 +1,61 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
+
+export const create = async (ctx) => {
+    
+    const password = await bcrypt.hash(ctx.request.body.password, 10)
+    
+    const data = {
+        name: ctx.request.body.name,
+        username: ctx.request.body.username,
+        email: ctx.request.body.email,
+        password
+    }
+
+    try {
+        const {password, ...user} = await prisma.user.create({ data })
+        ctx.body = user
+        ctx.status = 201
+    } catch (error) {
+        ctx.body = error
+        ctx.status = 500
+    }
+
+}
+
+export const login = async (ctx) => {
+    const [ type, token ] = ctx.headers.authorization.split(" ")
+
+    const [ email, plainTextPassword ] = atob(token).split(":")
+
+    const user = await prisma.user.findUnique({
+        where: { email }
+    })
+
+    if ( !user ) {
+        ctx.status = 404
+        return
+    }
+
+    const passwordMatch = await bcrypt.compare(plainTextPassword, user.password)
+
+    if ( !passwordMatch ) {
+        ctx.status = 404
+        return
+    }
+
+    const { password, ...result } = user
+    const accessToken = jwt.sign({
+        sub: user.id,
+        name: user.name,
+        expiresIn: "1d"
+    }, process.env.JWT_SECRET)
+
+    ctx.body = {
+        user: result,
+        accessToken
+    }
+}
